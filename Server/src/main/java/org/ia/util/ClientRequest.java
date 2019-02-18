a4159620c40e72b5799ddf246fc94353013e5efa
AdamBremholm <adambremholm@gmail.com>
2019-02-18 17:02:34 +0100

    Edit Folder Structure Add Plugin "Plugin"

--- Server/src/main/java/org/ia/util/ClientRequest.java
+++ Server/src/main/java/org/ia/util/ClientRequest.java
@@ -1,36 +1,10 @@
 package org.ia.util;
 
-
-import java.io.*;
-import java.net.Socket;
-import java.util.StringTokenizer;
-
 public class ClientRequest {
 
-    String firstLine;
     String method;
     String file = null;
     String accept;
-    String body = null;
-    Socket connect;
-    StringBuilder payload;
-    String contentType;
-
-    BufferedReader in = null;
-    PrintWriter out = null;
-    BufferedOutputStream dataOut = null;
-
-    public void initReaders() throws IOException {
-        in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-
-        // get first line of the request from the client
-        String input = in.readLine();
-        // we parse the request with a string tokenizer
-        StringTokenizer parse = new StringTokenizer(input);
-        method = parse.nextToken().toUpperCase(); // we get the HTTP method of the client
-        // we get file requested
-        file = parse.nextToken().toLowerCase();
-    }
 
     public String getContentType() {
         return contentType;
@@ -40,6 +14,8 @@ public class ClientRequest {
         this.contentType = contentType;
     }
 
+    String contentType;
+    String body = "";
 
     public ClientRequest(String method, String file, String accept, String body) {
         this.method = method;
@@ -48,10 +24,8 @@ public class ClientRequest {
         this.body = body;
     }
 
+    public ClientRequest() {
 
-
-    public ClientRequest(Socket connect) {
-        this.connect = connect;
     }
 
     public void setMethod(String method) {
@@ -90,25 +64,5 @@ public class ClientRequest {
         if (method.equals("POST")) {
             return true;
         } else return false;
-    }
-
-    public void readPost() throws IOException {
-        while ((body = in.readLine()).length() != 0){
-            System.out.println(body);
-        }
-
-        payload = new StringBuilder();
-        while(in.ready()){
-            payload.append((char) in.read());
-        }
-        System.out.println("Payload data is: "+getPayloadString());
-    }
-
-    public String getPayloadString() {
-        return payload.toString();
-    }
-
-    public void close() throws IOException {
-        in.close();
     }
 }

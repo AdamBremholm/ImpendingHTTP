223d210124308adebba4208824d7a7c4668d539f
AdamBremholm <adambremholm@gmail.com>
2019-02-19 20:35:29 +0100

    Edit initReader() to save post method body. RequestData

--- Server/src/main/java/org/ia/util/RequestData.java
+++ Server/src/main/java/org/ia/util/RequestData.java
@@ -10,8 +10,8 @@ public class RequestData {
     public RequestData() {
     }
 
-    String type;
-    String value;
+    private String type;
+    private String value;
 
     @Override
     public int hashCode() {
@@ -26,6 +26,22 @@ public class RequestData {
     @Override
     public String toString() {
         return type + ":" + value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 }
 

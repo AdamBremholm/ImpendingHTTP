65913e3f1254aee7df432c92fd92502470b56b09
AdamBremholm <skiryoland@gmail.com>
2019-02-22 11:44:04 +0100

    Merge with AdamBranch Update Webroot

--- Plugin/src/org/ia/plugin/TestPlugin.java
+++ Plugin/src/org/ia/plugin/TestPlugin.java
@@ -1,15 +1,16 @@
 package org.ia.plugin;
 
 import org.ia.api.Adress;
+import org.ia.api.ImpendingInterface;
 import org.ia.util.ClientRequest;
 import org.ia.util.ServerResponse;
-import org.ia.api.ImpendingInterface;
 
-@Adress("/v1/UpsertPlugin")
-public class UpsertPlugin implements ImpendingInterface {
+
+@Adress("/v1/TestPlugin")
+public class TestPlugin implements ImpendingInterface {
     @Override
     public ServerResponse execute(ClientRequest clientRequest, ServerResponse serverResponse) {
-        System.out.println("I'M CALLING FROM UPSERTPLUGIN");
+        System.out.println("hej!");
         return serverResponse;
     }
 }
--- Server/src/main/java/org/ia/util/ClientConnection.java
+++ Server/src/main/java/org/ia/util/ClientConnection.java
@@ -137,12 +137,14 @@ public class ClientConnection implements Runnable {
                         i++;
                     }
                 }
+
                 if (i==0) {
                     //Om inget plugin hittas, 404 file not found.
                     clientRequest.setFile("/" + ResourceConfig.FILE_NOT_FOUND);
                     serverResponse.setContentType(readFileData.getContentType(clientRequest.getFile()));
                     File file = new File(ResourceConfig.WEB_ROOT, clientRequest.getFile());
                     serverResponse.sendGet(file);
+
 
                 }
 

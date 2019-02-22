5f7f3d55ca72144e523eb2ea9ef21b4b1a68b819
Patrik <bagenholm.patrik@hotmail.com>
2019-02-22 13:25:43 +0100

    Add mongo cloud service

--- Plugin/src/org/ia/plugin/FindInDBPlugin.java
+++ Plugin/src/org/ia/plugin/FindInDBPlugin.java
@@ -5,13 +5,24 @@ import org.ia.api.ImpendingInterface;
 import org.ia.util.ClientRequest;
 import org.ia.util.ServerResponse;
 
+import java.io.IOException;
+
 import static org.ia.util.StorageController.storage;
 
-@Adress("v1/Person")
+@Adress("/v1/Person")
 public class FindInDBPlugin implements ImpendingInterface {
     @Override
     public ServerResponse execute(ClientRequest clientRequest, ServerResponse serverResponse) {
+        String searchResult = storage.findFirstPerson(clientRequest.getPayloadString());
         System.out.println(storage.findFirstPerson(clientRequest.getPayloadString()));
+        byte[] jsonbytes = searchResult.getBytes();
+        serverResponse.setJsonBytes(jsonbytes);
+        try {
+            serverResponse.sendPostJson();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return serverResponse;
     }
 }

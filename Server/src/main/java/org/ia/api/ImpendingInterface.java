9ed09c8f929a0a042d96196beee980e830725133
Patrik <bagenholm.patrik@hotmail.com>
2019-02-22 10:28:01 +0100

    Add db plugins, storage static

--- Server/src/main/java/org/ia/api/ImpendingInterface.java
+++ Server/src/main/java/org/ia/api/ImpendingInterface.java
@@ -1,13 +1,13 @@
 package org.ia.api;
 
 import org.ia.util.ClientRequest;
-import org.ia.util.RequestData;
 import org.ia.util.ServerResponse;
 
 public interface ImpendingInterface {
 
-    ServerResponse execute(ClientRequest clientRequest, ServerResponse serverResponse);
+    //ServerResponse execute(ClientRequest clientRequest, ServerResponse serverResponse);
     //Ska returna serverrespone
 
-    //TODO: Metod för att ta in ArrayList, Storage mm.
+    ServerResponse execute(ClientRequest clientRequest, ServerResponse serverResponse);
+
 }
